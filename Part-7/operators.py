60b39dd9ec0e7f7aa274f12aa28f472e2a8e8501
Abhigyan Jaiswal <abhigyanjj@gmail.com>
2020-12-30 17:42:03 +0900

    Added Part-7

---
+++ Part-7/operators.py
@@ -0,0 +1,23 @@
+#  Arithmetic Operators
+
++ # Plus
+- # Minus
+* # Multiply 
+% # Modulus
+/ # Division 
+// # Floor Division
+
+# Comparison Operators
+
+== # Equal To
+!= # Not Equal To
+< # Greater Than
+> # Less Than
+>= # Less Than Equal To
+<= # Greater Than Equal To
+
+# Logical Operators
+
+or # Or
+and # And
+not # Not
