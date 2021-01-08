1c8b574b7c274bd646472537ea1cff620cca4e7e
Abhigyan Jaiswal <abhigyanjj@gmail.com>
2021-01-08 18:45:09 +0900

    Added Part-9

---
+++ Part-9/if-else.py
@@ -0,0 +1,9 @@
+x = 10.0
+y = 10
+
+if x > y:
+    print(str(x) + " is greater the " + str(y))
+elif x < y:
+    print(str(x) + " is less the " + str(y))
+else:
+    print(str(x) + " is equal to " + str(y))
